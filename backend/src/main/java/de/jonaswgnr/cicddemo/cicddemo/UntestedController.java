34eaa92361d6c93fc38feebc0bbce59a234a307c
jonaswgnr <github@jonaswgnr.de>
2020-05-18 23:13:32 +0200

    Add CORS configuratioN

--- backend/src/main/java/de/jonaswgnr/cicddemo/cicddemo/UntestedController.java
+++ backend/src/main/java/de/jonaswgnr/cicddemo/cicddemo/UntestedController.java
@@ -1,11 +1,13 @@
 package de.jonaswgnr.cicddemo.cicddemo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UntestedController {
 
+    @CrossOrigin(origins="*")
     @RequestMapping("/demo/untested")
     public String index() {
         return "Demo Application for CI/CD Pipeline; I am untested";
