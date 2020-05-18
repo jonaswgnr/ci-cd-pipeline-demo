5651a771405225c58de42dc6a5f9be0f6f6edfc7
jonaswgnr <github@jonaswgnr.de>
2020-05-18 22:37:29 +0200

    Update Frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {stringify} from "querystring";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  apiURL: string = 'http://localhost:8181/demo';
+  constructor(private httpClient: HttpClient) { }
+
+  public getTestedEndpoint() {
+    return this.httpClient.get(`${this.apiURL}/tested`, {responseType: 'text'})
+  }
+  public getUntestedEndpoint(){
+    return this.httpClient.get(`${this.apiURL}/untested`, {responseType: 'text'})
+  }
+
 }
