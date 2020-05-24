f57a34f1bcdfbc18b0099ea18c012668c9aa5bb7
jonaswgnr <github@jonaswgnr.de>
2020-05-24 16:08:23 +0200

    Fix tests

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {stringify} from "querystring";
 
 
 @Component({
@@ -10,23 +9,23 @@ import {stringify} from "querystring";
 })
 export class AppComponent implements OnInit{
   title = 'frontend';
-  apiURL: string = 'http://localhost:8181/demo';
-  responseTested: string
-  responseUntested: string
+  apiURL = 'http://localhost:8181/demo';
+  responseTested: string;
+  responseUntested: string;
   constructor(private httpClient: HttpClient) {
 
   }
 
   public getTestedEndpoint() {
-    return this.httpClient.get(`${this.apiURL}/tested`, {responseType: 'text'})
+    return this.httpClient.get(`${this.apiURL}/tested`, {responseType: 'text'});
   }
   public getUntestedEndpoint(){
-    return this.httpClient.get(`${this.apiURL}/untested`, {responseType: 'text'})
+    return this.httpClient.get(`${this.apiURL}/untested`, {responseType: 'text'});
   }
 
   ngOnInit(): void {
-    this.getUntestedEndpoint().subscribe(data => {this.responseUntested = data})
-    this.getTestedEndpoint().subscribe(data => {this.responseTested = data})
+    this.getUntestedEndpoint().subscribe(data => {this.responseUntested = data; });
+    this.getTestedEndpoint().subscribe(data => {this.responseTested = data; });
   }
 
 }
